4c63d4f4c0234f6141327171c76364bd62d406f8
Hamzaabba2005 <hamza.abba@hotmail.com>
2025-03-16 23:28:27 +0000

    changes

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/checkmyplate/', // since you're deploying to checkmyplate.co.uk root domain
+  base: '/', // since you're deploying to checkmyplate.co.uk root domain
 })

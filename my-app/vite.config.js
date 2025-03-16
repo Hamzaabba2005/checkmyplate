54c03c2e87a8c4a4690620203a6879c14a4ef2a0
Hamzaabba2005 <hamza.abba@hotmail.com>
2025-03-16 23:41:08 +0000

    changes

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/checkmyplate/', // <-- add this line
+  base: '/', // <-- add this line
   plugins: [react()],
 })
